8475bddef971ef0210fafe7a8d961b30fd6a1d56
Christopher Smith <chrylis@gmail.com>
2018-03-19 22:37:32 -0500

    Trim Maven version output

--- vars/mavenVersion.groovy
+++ vars/mavenVersion.groovy
@@ -3,10 +3,10 @@
  * The tools section of a pipeline isn't run until after the environment
  * is evaluated, and using withMaven results in unhelpful extra stdout.
  */
-def call() {
+String call() {
   def version = sh(
     returnStdout: true,
     script: "mvn -q -Dexec.executable='echo' -Dexec.args='\${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.6.0:exec"
   )
-  return version
+  return version.trim()
 }
