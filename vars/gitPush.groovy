597ba69e8928a6c35a452e50f22473ebc62089fd
Christopher Smith <chrylis@gmail.com>
2018-03-23 01:53:53 -0500

    Genericize SSH key name

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -1,4 +1,4 @@
-def call(String sshKey='jenkins_github_ssh_key', String remote = UPSTREAM_REMOTE, String remoteBranch = UPSTREAM_BRANCH) {
+def call(String sshKey='git_ssh_key', String remote = UPSTREAM_REMOTE, String remoteBranch = UPSTREAM_BRANCH) {
   sshagent([sshKey]) {
     sh "git push ${remote} HEAD:${remoteBranch}"
   }
